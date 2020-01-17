91ccdcc48e5bd786b97f8b4fe964bb6132a1911a
YoSoyTonny <itonygame@gmail.com>
2020-01-17 07:49:41 -0700

    ya hay colisiones

---
+++ Assets/spritse/character2D.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class character2D : MonoBehaviour
+{
+    [SerializeField]
+    float moveSpeed = 5f;
+    //float moveSpeed {get, set};
+   void Update()
+   {
+      transform.Translate(Axis * moveSpeed * Time.deltaTime); 
+   }
+
+   Vector2 Axis 
+   {
+      get  => new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+   }
+
+}
